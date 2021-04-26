d43f581bc23effebeb4b6e10c5c1bf6b86ed2ea6
Mikołaj Mrózek <m.mik311@gmail.com>
2021-04-26 15:01:44 +0200

    Changed MoodLog file

--- components/MoodLog.tsx
+++ components/MoodLog.tsx
@@ -1,11 +1,31 @@
 import * as React from 'react'
-import EmotionName from '../constants/EmotionName';
-import dateNames from "../constants/CalendarNames"
-import { Text, } from 'react-native'
+import BasicContainer from "./BasicContainer"
+import Emotion from "../constants/EmotionEmoji"
+import CustomElementStyles from '../constants/CustomElementStyles';
+import { Text, View, Image } from 'react-native'
+import Colors from "../constants/Colors"
 
 export default function MoodLog(props: any) {
-    console.log(props[1]);
+    const MoodCart = () => {
+        console.log()
+        return (
+            <View style={{ marginBottom: -10 }}>
+                <View style={CustomElementStyles.mainHeader}>
+                    <Text style={CustomElementStyles.mainHeaderText}>{props.item[2].day}. {props.item[2].monthNameFull} {props.item[2].year}</Text>
+                </View>
+                <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between", marginTop: 4 }}>
+                    <View style={{ maxWidth: "100%" }}>
+                        <Text style={[CustomElementStyles.firstTabDescriptioText, { color: Colors.whiteOff }]}>Wtedy czułam/em się:</Text>
+                        <View style={{ marginTop: -4, marginBottom: 7, flex: 1, flexDirection: "row" }}>
+                            <Text style={[CustomElementStyles.firstTabDescriptioText, { color: Colors.tintColor }]} >{props.item[1]}</Text>
+                        </View>
+                    </View>
+                    <Image style={{ width: 70, height: 70, marginTop: -30 }} source={Emotion[props.item[0]]} />
+                </View>
+            </View>
+        );
+    }
     return (
-        <Text>{props[1]}</Text>
+        <BasicContainer content={<MoodCart />} />
     );
 }
--- navigation/NavigationHandler.tsx
+++ navigation/NavigationHandler.tsx
@@ -14,10 +14,7 @@ import BasicContainer from '../components/BasicContainer'
 import { throwIfAudioIsDisabled } from 'expo-av/build/Audio/AudioAvailability';
 // import MoodLog from '../components/MoodLog'
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import Emotion from "../constants/EmotionEmoji"
-import EmotionName from '../constants/EmotionName';
-import CustomElementStyles from '../constants/CustomElementStyles';
-// import MoodLog from '../components/MoodLog';
+import MoodLog from '../components/MoodLog';
 
 
 const getJSONData = async (storageKey: string) => {
@@ -46,30 +43,7 @@ function Icon(props: { name: React.ComponentProps<typeof Feather>['name']; color
     return <Feather size={30} style={{ marginTop: 8, marginRight: 0 }}{...props} />;
 }
 
-function MoodLog(props: any) {
-    const MoodCart = () => {
-        console.log()
-        return (
-            <View style={{ marginBottom: -10 }}>
-                <View style={CustomElementStyles.mainHeader}>
-                    <Text style={CustomElementStyles.mainHeaderText}>{props.item[2].day}. {props.item[2].monthNameFull} {props.item[2].year}</Text>
-                </View>
-                <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between", marginTop: 4 }}>
-                    <View style={{ maxWidth: "100%" }}>
-                        <Text style={[CustomElementStyles.firstTabDescriptioText, { color: Colors.whiteOff }]}>Wtedy czułam/em się:</Text>
-                        <View style={{ marginTop: -4, marginBottom: 7, flex: 1, flexDirection: "row" }}>
-                            <Text style={[CustomElementStyles.firstTabDescriptioText, { color: Colors.tintColor }]} >{props.item[1]}</Text>
-                        </View>
-                    </View>
-                    <Image style={{ width: 70, height: 70, marginTop: -30 }} source={Emotion[props.item[0]]} />
-                </View>
-            </View>
-        );
-    }
-    return (
-        <BasicContainer content={<MoodCart />} />
-    );
-}
+
 
 export default class NavigationHandler extends React.Component<IProps, IState> {
     constructor(props: any) {
