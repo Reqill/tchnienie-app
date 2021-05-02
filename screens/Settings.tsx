202d38a34a401f551dfd19b0bbc64f431b29652f
Mikołaj Mrózek <m.mik311@gmail.com>
2021-05-02 18:20:07 +0200

    Added new settings

--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -1,19 +1,15 @@
 import * as React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import CustomElementStyles from '../constants/CustomElementStyles';
-import { Text, View, Image, TouchableOpacity, Alert, Button } from 'react-native'
+import { Text, View, Image, TouchableOpacity, Alert, Button, Touchable } from 'react-native'
 import Colors from "../constants/Colors"
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import CourseList from '../constants/CourseList'
 import MusicList from '../constants/MusicList'
+import { Feather } from '@expo/vector-icons';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const courseDataBlank = {
-    isWatched: false,
-    isLiked: false,
-    isCompleted: false,
-    isActive: false,
-    watchedEpisodes: 0,
-}
+
 const moodDataBlank = null;
 const streakDataBlank = null;
 
@@ -26,32 +22,112 @@ const storeJSON = async (storageKey: string, value: JSON | Array<any>) => {
     }
 }
 
-const blankMusicData = () => {
-    const musicDataBlank = {
-        isLiked: false,
+export default function Settings() {
+
+    const wipeCourseData = () => {
+        const courseDataBlank = {
+            isWatched: false,
+            isLiked: false,
+            isCompleted: false,
+            isActive: false,
+            watchedEpisodes: 0,
+        }
+        storeJSON(`course-${CourseList[0].name}-${CourseList[0].id}`, courseDataBlank)
+        storeJSON(`course-${CourseList[1].name}-${CourseList[1].id}`, courseDataBlank)
+        storeJSON(`course-${CourseList[2].name}-${CourseList[2].id}`, courseDataBlank)
+        storeJSON(`course-${CourseList[3].name}-${CourseList[3].id}`, courseDataBlank)
     }
 
-    storeJSON(`music-${MusicList[0].title}-${MusicList[0].id}`, musicDataBlank);
-    storeJSON(`music-${MusicList[1].title}-${MusicList[1].id}`, musicDataBlank);
-    storeJSON(`music-${MusicList[2].title}-${MusicList[2].id}`, musicDataBlank);
-    storeJSON(`music-${MusicList[3].title}-${MusicList[3].id}`, musicDataBlank);
-    storeJSON(`music-${MusicList[4].title}-${MusicList[4].id}`, musicDataBlank);
-    storeJSON(`music-${MusicList[5].title}-${MusicList[5].id}`, musicDataBlank);
-    storeJSON(`music-${MusicList[6].title}-${MusicList[6].id}`, musicDataBlank);
+    const wipeMusicData = () => {
+        const musicDataBlank = {
+            isLiked: false,
+        }
+        storeJSON(`music-${MusicList[0].title}-${MusicList[0].id}`, musicDataBlank);
+        storeJSON(`music-${MusicList[1].title}-${MusicList[1].id}`, musicDataBlank);
+        storeJSON(`music-${MusicList[2].title}-${MusicList[2].id}`, musicDataBlank);
+        storeJSON(`music-${MusicList[3].title}-${MusicList[3].id}`, musicDataBlank);
+        storeJSON(`music-${MusicList[4].title}-${MusicList[4].id}`, musicDataBlank);
+        storeJSON(`music-${MusicList[5].title}-${MusicList[5].id}`, musicDataBlank);
+        storeJSON(`music-${MusicList[6].title}-${MusicList[6].id}`, musicDataBlank);
+    }
+
+    const wipeAppData = () => {
+        storeJSON(`moodWholeData`, moodDataBlank);
+        storeJSON(`dailyStreakSeries`, streakDataBlank);
+        wipeMusicData();
+        wipeCourseData();
+    }
+
+
+    const reportBug = () => {
+        console.log("Zgłoś błąd")
+    }
+
+    const rateApp = () => {
+        console.log("Oceń aplikacje")
+    }
+
+    const fillForm = () => {
+        console.log("Wypełnij ankietę")
+    }
+
+    const authorsAndProject = () => {
+        console.log("Twórcy i projekt")
+    }
+
+    const partners = () => {
+        console.log("Partnerzy")
+    }
+
+    return (
+
+        <View style={{ width: "100%" }}>
+            <ScrollView>
+                <View style={{ marginLeft: "5%", paddingBottom: 85 }}>
+                    <Text style={CustomElementStyles.settingsIndicator}>OGÓLNE</Text>
+                    <SettingBar iconName="x-square" buttonText="Usuń dane o kursach" handleClick={wipeCourseData} />
+                    <SettingBar iconName="x-square" buttonText="Usuń dane o muzyce" handleClick={wipeMusicData} />
+                    <SettingBar iconName="x-square" buttonText="Usuń dane aplikacji" handleClick={wipeAppData} />
+                    <Text style={CustomElementStyles.settingsIndicator}>DANE</Text>
+                    <SettingBar iconName="x-square" buttonText="Usuń dane o kursach" handleClick={wipeCourseData} />
+                    <SettingBar iconName="x-square" buttonText="Usuń dane o muzyce" handleClick={wipeMusicData} />
+                    <SettingBar iconName="x-square" buttonText="Usuń dane aplikacji" handleClick={wipeAppData} />
+                    <Text style={CustomElementStyles.settingsIndicator}>INNE</Text>
+                    <SettingBar iconName="flag" buttonText="Zgłoś błąd" handleClick={reportBug} />
+                    <SettingBar iconName="thumbs-up" buttonText="Oceń aplikacje" handleClick={rateApp} />
+                    <SettingBar iconName="check-square" buttonText="Wypełnij ankietę" handleClick={fillForm} />
+                    <SettingBar iconName="info" buttonText="Twórcy i projekt" handleClick={authorsAndProject} />
+                    <SettingBar iconName="info" buttonText="Partnerzy" handleClick={partners} />
+                </View>
+            </ScrollView>
+        </View>
+
+
+    );
 }
 
-export default function Settings() {
+
+const SettingBar = (props: { iconName: React.ComponentProps<typeof Feather>['name'], buttonText: string, handleClick: Function }) => {
     return (
         <View>
-            <Text>Ustawienia</Text>
-            <Button title={"Reset Kurs 1"} onPress={() => storeJSON(`course-${CourseList[0].name}-${CourseList[0].id}`, courseDataBlank)} />
-            <Button title={"Reset Kurs 2"} onPress={() => storeJSON(`course-${CourseList[1].name}-${CourseList[1].id}`, courseDataBlank)} />
-            <Button title={"Reset Kurs 3"} onPress={() => storeJSON(`course-${CourseList[2].name}-${CourseList[2].id}`, courseDataBlank)} />
-            <Button title={"Reset Kurs 4"} onPress={() => storeJSON(`course-${CourseList[3].name}-${CourseList[3].id}`, courseDataBlank)} />
-            <Button title={"Reset Music"} onPress={() => blankMusicData()} />
-            <Button title={"Reset Mood Log"} onPress={() => storeJSON(`moodWholeData`, moodDataBlank)} />
-            <Button title={"Reset Streak Log"} onPress={() => storeJSON(`dailyStreakSeries`, streakDataBlank)} />
+            <TouchableOpacity
+                onPress={() => props.handleClick()}
+                style={{ flex: 1, flexDirection: "row", alignItems: "center", marginBottom: 18, marginLeft: 6, marginTop: 1 }}
+            >
+                <Icon name={props.iconName} color={Colors.whiteOff} size={30} />
+                <Text
+                    style={{
+                        color: Colors.whiteOff, fontSize: 17, marginTop: 3.5,
+                        fontFamily: "Poppins_400Regular", marginLeft: 10
+                    }}
+                >
+                    {props.buttonText}
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 }
 
+function Icon(props: { name: React.ComponentProps<typeof Feather>['name']; color: string; size: number }) {
+    return <Feather style={{ marginTop: .5 }}{...props} />;
+}
