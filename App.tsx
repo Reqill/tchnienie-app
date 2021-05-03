f1dcc9b653a081b2cdc6088b4aa39b0286855e46
Mikołaj Mrózek <m.mik311@gmail.com>
2021-05-04 00:12:43 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { View } from 'react-native';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -8,10 +9,12 @@ import Navigation from './navigation';
 import NavigationHandler from './navigation/NavigationHandler'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import FirstLaunch from './screens/FirstLaunch';
+import Colors from './constants/Colors'
 
 
 import { useFonts, Poppins_500Medium, Poppins_600SemiBold, Poppins_400Regular, Poppins_300Light } from '@expo-google-fonts/poppins';
 import { Pacifico_400Regular } from '@expo-google-fonts/pacifico';
+import { withDecay } from 'react-native-reanimated';
 
 export default function App() {
   const isLoadingComplete = useCachedResources(); //alredy checked
@@ -28,6 +31,22 @@ export default function App() {
     Pacifico_400Regular
   });
 
+  const date = new Date();
+  const start = new Date(date.getFullYear(), 0, 0);
+  const diff = date - start;
+  const oneDay = 1000 * 60 * 60 * 24;
+  const dayOfYear = Math.floor(diff / oneDay);
+  AsyncStorage.getItem('LastDayOpen').then((res) => {
+    if (res === null) {
+      AsyncStorage.setItem('LastDayOpen', String(dayOfYear - 1))
+    } else if (Number(res) !== dayOfYear) {
+      AsyncStorage.setItem('LastDayOpen', String(dayOfYear - 1))
+    }
+
+  })
+
+
+
   if (!isLoadingComplete && !fontsLoaded) {
     return null;
   } else {
@@ -41,16 +60,15 @@ export default function App() {
     );
   }
 }
-
 class StartingComponent extends React.Component {
   constructor(props: any) {
     super(props);
     this.state = { firstLaunch: null };
   }
   componentDidMount() {
-    AsyncStorage.getItem("alreadyLaunchedd1").then(value => {
+    AsyncStorage.getItem("alreadyLaunchedd28").then(value => {
       if (value === null) {
-        AsyncStorage.setItem('alreadyLaunchedd1', String(true)); // No need to wait for `setItem` to finish, although you might want to handle errors
+        AsyncStorage.setItem('alreadyLaunchedd28', String(true)); // No need to wait for `setItem` to finish, although you might want to handle errors
         this.setState({ firstLaunch: true });
       }
       else {
@@ -63,11 +81,16 @@ class StartingComponent extends React.Component {
     if (this.state.firstLaunch === null) {
       return null; // This is the 'tricky' part: The query to AsyncStorage is not finished, but we have to present something to the user. Null will just render nothing, so you can also put a placeholder of some sort, but effectively the interval between the first mount and AsyncStorage retrieving your data won't be noticeable to the user.
     } else if (this.state.firstLaunch == true) {
-      return <FirstLaunch />
+      return (
+        <SafeAreaProvider>
+          <StatusBar />
+          <View style={{ backgroundColor: Colors.backgroundColor, width: "100%", height: "100%" }}>
+            <FirstLaunch />
+          </View>
+        </SafeAreaProvider>);
     } else {
       return (
         <SafeAreaProvider>
-          {/* <Navigation colorScheme={colorScheme} /> */}
           <NavigationHandler />
           <StatusBar />
         </SafeAreaProvider>
